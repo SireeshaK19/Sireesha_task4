43329af945d13a1d58d1966b427fdb530f702bfb
SireeshaK19 <60291509+SireeshaK19@users.noreply.github.com>
2020-02-20 22:04:49 +0530

    Add files via upload

---
+++ com.taskk4/src/main/java/Presentation_layer/Presentation.java
@@ -0,0 +1,6 @@
+package Presentation_layer;
+
+public interface Presentation {
+	int Compound_Interest_Action(int Principle_Amount, int Rate_Of_Interest, int Time );
+	int Simple_Interest_Action(int Principle_Amount, int Rate_Of_Interest, int Time );
+}
---
+++ com.taskk4/src/main/java/client_file/constructions.java
@@ -0,0 +1,5 @@
+package client_file;
+
+public interface constructions {
+	String Constructions_Materials(String Material_Standard,int Area );
+}
